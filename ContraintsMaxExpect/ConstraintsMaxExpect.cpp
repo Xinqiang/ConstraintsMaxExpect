560a3e74cdb38ab4c256c8458aacc7276fd8482d
ding xin qiang <xinqiang@dongtinglulab.local>
2012-11-18 20:53:55 +0800

    Both readpfsave and calculateprobability work.

--- ContraintsMaxExpect/ConstraintsMaxExpect.cpp
+++ ContraintsMaxExpect/ConstraintsMaxExpect.cpp
@@ -14,25 +14,53 @@
 using namespace std;
 int main(int argc, const char * argv[])
 {
-    structure *ct;
-    double *w5;
-    double *w3;
-    pfunctionclass *v;
-    pfunctionclass *w;
-    pfunctionclass *wmb;
-    pfunctionclass *wl;
-    pfunctionclass *wmbl;
-    pfunctionclass *wcoax;
-    forceclass *fce;
-    double *scaling;
-    bool *mod;
-    bool *lfce;
-    pfdatatable *data;
+    ifstream pfsin("/Users/xinqiang/Research/RNASecondaryStructure/ContraintsMaxExpect/ContraintsMaxExpect/test.pfs", ios::in | ios::binary);
+    
+    if(!pfsin){
+        cout << "Cannot open file.\n";
+        return 1;
+    }
+    short vers;
+    int  numofbases;
+    
+    pfsin.read((char *) &vers, sizeof vers);
+    
+    cout << vers << "\n"; 
+    
+    structure *ct = new structure;
+    
+    pfsin.read((char *) &ct->numofbases, sizeof numofbases);
+    
+    cout << ct->numofbases << "\n";
+    
+    ct->allocate(ct->numofbases);
+    
+    double *w5 = new double [ct->numofbases + 1];
+    double *w3 = new double [ct->numofbases + 2];
+    pfunctionclass *v = new pfunctionclass(ct->numofbases);
+    pfunctionclass *w = new pfunctionclass(ct->numofbases);
+    pfunctionclass *wmb = new pfunctionclass(ct->numofbases);
+    pfunctionclass *wl = new pfunctionclass(ct->numofbases);
+    pfunctionclass *wmbl = new pfunctionclass(ct->numofbases);
+    pfunctionclass *wcoax = new pfunctionclass(ct->numofbases);
+    forceclass *fce = new forceclass(ct->numofbases);
+    double *scaling = new double;
+    bool *mod = new bool [2*ct->numofbases + 1];
+    bool *lfce = new bool [2*ct->numofbases + 1];
+    pfdatatable *data = new pfdatatable();
     
     readpfsave("/Users/xinqiang/Research/RNASecondaryStructure/ContraintsMaxExpect/ContraintsMaxExpect/test.pfs", ct, w5, w3, v, w, wmb, wl, wmbl, wcoax, fce, scaling, mod, lfce, data);
     
-    cout << w3[1];
-    cout << "Hello, World!\n";
+    cout << w3[1] << "\n" << ct->nucs << "\n";
+    cout << *scaling << "\n";
+    int i,j;
+    
+    for (i=1; i<=ct->numofbases; i++){
+        for (j=i+1; j<=ct->numofbases; j++){
+            cout << calculateprobability(i,j, v, w5, ct, data, lfce, mod, *scaling, fce) << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
 
